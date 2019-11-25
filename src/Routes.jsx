16f4c14cdc0bbef0f1618827f8134f2d28cc1dc4
UnDer-7 <mateus7532@gmail.com>
2019-11-25 08:27:34 -0300

    Fix

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Redirect from 'react-router-dom/es/Redirect';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
 import ClienteForm from './pages/cliente/ClienteForm';
+import { getToken } from './services/auth.service';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact>
+          {getToken() == null ? <Redirect to="/login" /> : <Home />}
+        </Route>
         <Route path="/login" component={Login} />
         <Route path="/cliente/new" component={ClienteForm} />
         <Route path="/cliente/edit/:id" component={ClienteForm} />
